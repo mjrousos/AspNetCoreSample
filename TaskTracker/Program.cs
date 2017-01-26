53835ce641e1a8212210613189d9f7dd8a0b68fc
Mike Rousos <MikeRou@Microsoft.com>
2017-01-26 13:48:23 -0800

    Listen on all URLs (both IP4 and IP6) with Kestrel, not just localhost

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -13,8 +13,29 @@ namespace TaskList
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+
+                // UseUrls can specify which URLs Kestrel listens to.
+                // Notice that we can specify multiple URLs.
+                //
+                // http://+ will listen on all URLs, so it will enable the app to
+                // listen for names other than localhost/127.0.0.1 (which will be 
+                // useful once the app is deployed)
+                // https://msdn.microsoft.com/en-us/library/aa364698(v=vs.85).aspx
+                //
+                // Note that * is preferred to 0.0.0.0 because * will listen on IP4 /and/ IP6 address instead of on
+                // just IP4.
+                //
+                // This call MUST come before UseIISIntegration (if IIS integration is used)
+                // 
+                // URLs can also be picked up from a config file, if needed 
+                // http://stackoverflow.com/questions/34212765/how-do-i-get-the-kestrel-web-server-to-listen-to-non-localhost-requests
+                .UseUrls("http://+:5000", "http://+:5001")
+                
                 .UseContentRoot(Directory.GetCurrentDirectory())
+
+                // Note that UseIISIntegration MUST come after UseUrls so that the URLs IIS integration sets aren't overridden
                 .UseIISIntegration()
+
                 .UseStartup<Startup>()
                 .Build();
 
