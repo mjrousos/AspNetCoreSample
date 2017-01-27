bc77e361e36427c6b3516fca01f9fd1296ef0c6d
Mike Rousos <MikeRou@Microsoft.com>
2017-01-26 19:51:48 -0800

    Add initial migration for sql db

---
+++ TaskTracker/Migrations/20170127034841_InitialMigration.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskTracker.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskCategory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Complete = table.Column<bool>(nullable: false),
+                    Completed = table.Column<DateTime>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItem", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskItemXTaskCategory",
+                columns: table => new
+                {
+                    TaskItemId = table.Column<Guid>(nullable: false),
+                    TaskCategoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItemXTaskCategory", x => new { x.TaskItemId, x.TaskCategoryId });
+                    table.ForeignKey(
+                        name: "FK_TaskItemXTaskCategory_TaskCategory_TaskCategoryId",
+                        column: x => x.TaskCategoryId,
+                        principalTable: "TaskCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskItemXTaskCategory_TaskItem_TaskItemId",
+                        column: x => x.TaskItemId,
+                        principalTable: "TaskItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItemXTaskCategory_TaskCategoryId",
+                table: "TaskItemXTaskCategory",
+                column: "TaskCategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskItemXTaskCategory");
+
+            migrationBuilder.DropTable(
+                name: "TaskCategory");
+
+            migrationBuilder.DropTable(
+                name: "TaskItem");
+        }
+    }
+}
---
+++ TaskTracker/Migrations/TasksContextModelSnapshot.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TaskList.Models;
+
+namespace TaskTracker.Migrations
+{
+    [DbContext(typeof(TasksContext))]
+    partial class TasksContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TaskList.Models.TaskCategory", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Created");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("TaskCategory");
+                });
+
+            modelBuilder.Entity("TaskList.Models.TaskItem", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("Complete");
+
+                    b.Property<DateTime?>("Completed");
+
+                    b.Property<DateTime>("Created");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("TaskItem");
+                });
+
+            modelBuilder.Entity("TaskList.Models.TaskItemXTaskCategory", b =>
+                {
+                    b.Property<Guid>("TaskItemId");
+
+                    b.Property<Guid>("TaskCategoryId");
+
+                    b.HasKey("TaskItemId", "TaskCategoryId");
+
+                    b.HasIndex("TaskCategoryId");
+
+                    b.ToTable("TaskItemXTaskCategory");
+                });
+
+            modelBuilder.Entity("TaskList.Models.TaskItemXTaskCategory", b =>
+                {
+                    b.HasOne("TaskList.Models.TaskCategory", "Category")
+                        .WithMany("TaskJoins")
+                        .HasForeignKey("TaskCategoryId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("TaskList.Models.TaskItem", "Task")
+                        .WithMany("CategoryJoins")
+                        .HasForeignKey("TaskItemId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
